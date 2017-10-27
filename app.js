10cabca45d7bd6a3a44a28b1540f06f2441251bc
Michael Sung <michaelsung@users.noreply.github.com>
2017-10-27 23:16:40 +1100

    add eslint

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express');
+
 const app = express();
 const axios = require('axios');
 
-app.get('/api/random', function (req, res) {
+app.get('/api/random', (req, res) => {
   axios.get('https://www.reddit.com/r/SimpsonsFaces/random.json')
     .then((response) => {
-      const object = response.data[0].data.children[0].data; // take the reddit post out of the response
+      // take the reddit post out of the response
+      const object = response.data[0].data.children[0].data;
       const url = object.url;
       const title = object.title;
 
       const returnJSON = {
-        "caption": title,
-        "url": url,
-      }
-      
+        caption: title,
+        url,
+      };
+
       res.send(returnJSON);
     });
 });
