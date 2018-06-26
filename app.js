b448585d545d6aebc6cf30f9d6efe1246a727692
Michael Sung <michaelsung@users.noreply.github.com>
2018-06-27 02:16:11 +1000

    add cors

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const axios = require('axios');
 
+app.use(cors());
+
 app.get('/api/random', (req, res) => {
-  axios.get('https://www.reddit.com/r/SimpsonsFaces/random.json')
-    .then((response) => {
-      // take the reddit post out of the response
-      const object = response.data[0].data.children[0].data;
-      const { url, title } = object;
+	axios.get('https://www.reddit.com/r/SimpsonsFaces/random.json')
+		.then((response) => {
+			// take the reddit post out of the response
+			const object = response.data[0].data.children[0].data;
+			// console.log(object.preview.images[0].source.url);
+			const url = object.preview.images[0].source.url;
+			const { title } = object;
 
-      const returnJSON = {
-        caption: title,
-        url,
-      };
+			const returnJSON = {
+				caption: title,
+				url,
+			};
 
-      res.send(returnJSON);
-    });
+			res.send(returnJSON);
+		});
 });
 
 app.listen(3000, () => {
-  console.log('Ready to SIMPS!!!!');
+	console.log('Ready to SIMPS!!!!');
 });
